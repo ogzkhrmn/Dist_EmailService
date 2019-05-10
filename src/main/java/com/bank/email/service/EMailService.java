ffba291575037e9ff19c9413ad46a1d47644069b
okahraman <oguz.kahraman@adesso.com.tr>
2019-05-10 22:45:19 +0300

    added response

--- src/main/java/com/bank/email/service/EMailService.java
+++ src/main/java/com/bank/email/service/EMailService.java
@@ -2,6 +2,7 @@ package com.bank.email.service;
 
 import com.bank.email.dao.impl.MailSendDAO;
 import com.bank.email.model.RequestModel;
+import com.bank.email.model.ResponseModel;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -17,44 +18,58 @@ public class EMailService implements Serializable {
     @Path("/sendMail")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public void sendEmail(RequestModel request) {
+    public ResponseModel sendEmail(RequestModel request) {
         MailSendDAO mailSendDAO = new MailSendDAO();
         mailSendDAO.sendMail(request);
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.setSuccess(true);
+        return responseModel;
     }
 
     @POST
     @Path("/sendOkMail")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public void sendOkMail(RequestModel request) {
+    public ResponseModel sendOkMail(RequestModel request) {
         MailSendDAO mailSendDAO = new MailSendDAO();
         mailSendDAO.sendOkMail(request);
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.setSuccess(true);
+        return responseModel;
     }
 
     @POST
     @Path("/sendErrortoUser")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public void sendErrortoUser(RequestModel request) {
+    public ResponseModel sendErrortoUser(RequestModel request) {
         MailSendDAO mailSendDAO = new MailSendDAO();
         mailSendDAO.sendErrortoUser(request);
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.setSuccess(true);
+        return responseModel;
     }
 
     @POST
     @Path("/sendErrorMail")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public void sendErrorMail(RequestModel request) {
+    public ResponseModel sendErrorMail(RequestModel request) {
         MailSendDAO mailSendDAO = new MailSendDAO();
         mailSendDAO.sendErrorMail(request);
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.setSuccess(true);
+        return responseModel;
     }
 
     @POST
     @Path("/control")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public boolean isAlive() {
-        return true;
+    public ResponseModel isAlive() {
+        ResponseModel responseModel = new ResponseModel();
+        responseModel.setSuccess(true);
+        return responseModel;
     }
 
 }
